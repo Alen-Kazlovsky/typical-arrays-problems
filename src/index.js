387c878e0c8a507a4af581fb1a9cf7264feef1b7
Alen Kazlovsky <49515281+Alen-Kazlovsky@users.noreply.github.com>
2021-08-23 21:03:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-exports.min = function min(array) {
-    if (!array || !array.length) return 0;
-    return Math.min(...array);
-};
+exports.min = function min (array) {
+  if ( arguments.length == 0 || array.length == 0 ) {
+    return 0;
+  };
+  let result = Math.min.apply(null, array);
+  return result;
+}
 
-exports.max = function max(array) {
-    if (!array || !array.length) return 0;
-    return Math.max(...array);
-};
+exports.max = function max (array) {
+  if ( arguments.length == 0 || array.length == 0) {
+    return 0;
+  };
+  let result = Math.max.apply(null, array);
+  return result;
+}
 
-exports.avg = function avg(array) {
-    if (!array || !array.length) return 0;
-    return array.reduce((sum, item) => sum + item, 0) / array.length;
-};
+exports.avg = function avg (array) {
+  if ( arguments.length == 0 || array.length == 0) {
+    return 0;
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
+}
